42377165e6fcdadba6dd012961729751aa10151e
David Lewis and Emily Watkins <david+emily@>
2018-04-25 11:42:20 -0700

    add home page with sign in / sign out functionality.

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ group :development, :test do
   gem 'pry'
   gem 'shoulda-matchers'
   gem 'factory_bot_rails'
+  gem 'capybara'
+  gem 'simplecov', require: false
 end
 
 group :development do
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "You've successfully signed up!"
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      flash[:alert] = "There was a problem signing up."
+      redirect_to new_user_registration_path
+    end
+  end
+end
