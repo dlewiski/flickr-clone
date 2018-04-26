11433a151b5f3f41ce4996c44d1a2263b65f090f
David Lewis and Emily Watkins <david+emily@>
2018-04-26 11:55:46 -0700

    create posts controller and new view to add post to the database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
+    @posts = Post.includes(:user)
   end
 end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      flash[:notice] = "Post successfully posted yaaa"
+      redirect_to user_posts_path(current_user)
+    else
+      render:new
+    end
+  end
+
+  def show
+    @posts = Post.all
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :image)
+  end
+end
